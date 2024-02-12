4ee5a98e72da11611a2441cbcb71eeacb96e8e6c
ctsetera <ctsetera@proton.me>
2024-02-12 12:44:26 +0900

    依存関係を更新

--- Sample01/build.gradle.kts
+++ Sample01/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     }
     repositories {
         google()
